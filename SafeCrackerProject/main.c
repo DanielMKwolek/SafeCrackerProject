794c60689ec002448979732ca1fa7485a2e983f8
Daniel Kwolek <Daniel@daniels-macbook-pro.local>
2016-08-10 21:40:16 -0400

    Initial Commit

---
+++ SafeCrackerProject/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SafeCrackerProject
+//
+//  Created by Daniel Kwolek on 8/10/16.
+//  Copyright © 2016 Arcore. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
